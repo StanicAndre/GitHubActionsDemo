51f65f8347050dfa801ce421d50225186c2882f5
StanicAndre <147137335+StanicAndre@users.noreply.github.com>
2023-11-14 10:04:33 +0100

    Create delete_workflow_artifact.ps1

---
+++ powershell/delete_workflow_artifact.ps1
@@ -0,0 +1,37 @@
+# parameters required to call the API
+param(
+    [Parameter(Position=0, Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $github_token
+)
+param(
+    [Parameter(Position=1, Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $run_id
+)
+
+# set preferences
+$ErrorActionPreference = "Stop" 
+$reporootpath = (get-item $PSScriptRoot ).parent.FullName;
+Set-Location $reporootpath;
+
+# Variable declaration for api calls
+$header = @{};
+$header.Add("Authorization", "Bearer $github_token");
+$header.Add("Accept", "application/vnd.github+json");
+$header.Add("X-GitHub-Api-Version", "2022-11-28");
+[string]$repo_url = "https://api.github.com/repos/StanicAndre/GitHubActionsDemo/actions/runs/$run_id/artifacts"
+
+# get existing artifacts
+$artifact = Invoke-RestMethod -Uri $repo_url -Headers $header -Method Get;
+
+if($artifacts.total.count -ge 1)
+{ 
+    #delete artifact   
+    $artifact_id = $artifact.artifacts.id
+    Write-Host "delete artifact `"$artifact_id`""
+    Invoke-RestMethod -Uri $artifact.artifacts.url -Headers $header -Method Delete;
+    
+}
