f9227e4cac0ec180fb4b2f411217c77565c78273
StanicAndre <147137335+StanicAndre@users.noreply.github.com>
2023-11-16 09:44:09 +0100

    Create upload_artifacts.ps1

---
+++ powershell/upload_artifacts.ps1
@@ -0,0 +1,28 @@
+# parameters required to call the API
+param(
+    [Parameter(Position=0, Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $repository,
+
+    [Parameter(Position=1, Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $github_token
+)
+
+# set preferences
+$ErrorActionPreference = "Stop" 
+$reporootpath = (get-item $PSScriptRoot ).parent.FullName;
+Set-Location $reporootpath;
+
+# Variable declaration for api calls
+$header = @{};
+$header.Add("Authorization", "Bearer $github_token");
+$header.Add("Accept", "application/vnd.github+json");
+$header.Add("X-GitHub-Api-Version", "2022-11-28");
+[string]$repo_url = "https://api.github.com/repos/StanicAndre/$repository/actions/artifacts" 
+
+#create artifact
+$file_Path =  "D:\a\GitHubActionsDemo\GitHubActionsDemo\README.md"
+$create_art = Invoke-RestMethod -Uri $repo_url -Headers $header -Method Post -InFile $file_Path
